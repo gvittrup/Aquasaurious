f0cc9d938f527e65cbef9578c58391a8c6ea6e6d
gvittrup <gunnar.vittrup@gmail.com>
2023-07-03 11:41:25 -0600

    Camera Follow + Bounds

--- Aquasaurious/Assets/Scripts/PlayerMovement.cs
+++ Aquasaurious/Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 4.0f;
+    private float playerSpeed = 5.0f;
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
 
