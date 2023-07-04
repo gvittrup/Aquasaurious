05373bd8280fd3a2075b75fc94d62303ba1421ee
Matthew Moldovan <120879285+MatthewJMoldovan@users.noreply.github.com>
2023-07-03 22:55:52 -0600

    Create random object spawner

---
+++ Aquasaurious/Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+
+    public GameObject cubePrefab;
+    public bool stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObject",spawnTime,spawnDelay);
+    }
+
+    public void SpawnObject() {
+        Vector3 randomSpawnPosition = new Vector3(0,Random.Range(-10,11),0);
+        Instantiate(cubePrefab,randomSpawnPosition,Quaternion.identity);
+        if(stopSpawning) {
+            CancelInvoke("SpawnObject");
+        }
+    }
+
+}
