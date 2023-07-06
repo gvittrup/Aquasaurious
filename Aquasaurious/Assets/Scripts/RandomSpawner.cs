b46c3124422d8860ca02a38995f44ab4dfa2c212
Matthew Moldovan <120879285+MatthewJMoldovan@users.noreply.github.com>
2023-07-06 12:58:15 -0600

    Added movement for fish / cleaned up code

--- Aquasaurious/Assets/Scripts/RandomSpawner.cs
+++ Aquasaurious/Assets/Scripts/RandomSpawner.cs
@@ -9,11 +9,13 @@ public class RandomSpawner : MonoBehaviour
     public bool stopSpawning = false;
     public float spawnTime;
     public float spawnDelay;
+    // public float speed = 1;
 
     void Start()
     {
         InvokeRepeating("SpawnObject",spawnTime,spawnDelay);
     }
+    
 
     public void SpawnObject() {
         Vector3 randomSpawnPosition = new Vector3(0,Random.Range(-10,11),0);
