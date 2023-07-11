c2abf1f86b062e84d45a25d0ccc8d07925cf9290
gvittrup <gunnar.vittrup@gmail.com>
2023-07-11 10:45:11 -0600

    Cleaning `PlayerMovement`

--- Aquasaurious/Assets/Scripts/PlayerMovement.cs
+++ Aquasaurious/Assets/Scripts/PlayerMovement.cs
@@ -20,12 +20,6 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        // groundedPlayer = controller.isGrounded;
-        // if (groundedPlayer && playerVelocity.y < 0)
-        // {
-        //     playerVelocity.y = 0f;
-        // }
-
 
         /* This handles the physical movement of the gameobject */
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
@@ -37,15 +31,5 @@ public class PlayerMovement : MonoBehaviour
             gameObject.transform.forward = move;
         }
 
-
-
-        // Changes the height position of the player..
-        // if (Input.GetButtonDown("Jump") && groundedPlayer)
-        // {
-        //     playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        // }
-
-        // playerVelocity.y += gravityValue * Time.deltaTime;
-        // controller.Move(playerVelocity * Time.deltaTime);
     }
 }
