3e0d71de4ab397e403cd1b19954b9d01b5d7bca0
gvittrup <gunnar.vittrup@gmail.com>
2023-07-12 19:37:37 -0600

    Coral + Box + Bottle

--- Aquasaurious/Assets/Scripts/RandomSpawner.cs
+++ Aquasaurious/Assets/Scripts/RandomSpawner.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class RandomSpawner : MonoBehaviour
 {
 
-    public GameObject cubePrefab;
+    public GameObject[] objects;
+    public GameObject fish, ground;
     public bool stopSpawning = false;
     public float spawnTime;
     public float spawnDelay;
-    // public float speed = 1;
+
+    int[] zRanges = new int[] {-15, -10, 0, 15};
 
     void Start()
     {
@@ -18,8 +20,20 @@ public class RandomSpawner : MonoBehaviour
     
 
     public void SpawnObject() {
-        Vector3 randomSpawnPosition = new Vector3(20, Random.Range(-10,11),0);
-        Instantiate(cubePrefab,randomSpawnPosition,Quaternion.identity);
+
+        Vector3 randomSpawnPosition = new Vector3(50, Random.Range(-10,11),0);
+        Instantiate(fish,randomSpawnPosition,Quaternion.identity);
+
+        //Handles spawning of atmosphere elements, such as bottles and coral
+        int randomObject = Random.Range(0,3);
+        int randomZ = Random.Range(0,4);
+        if(objects[randomObject].gameObject.tag == "Ground") {
+            randomSpawnPosition = new Vector3(50, ground.transform.position.y, zRanges[randomZ]);
+        } else {
+            randomSpawnPosition = new Vector3(50, Random.Range(-8,8), zRanges[randomZ]);
+        }
+        Instantiate(objects[randomObject], randomSpawnPosition, Quaternion.identity);
+
         if(stopSpawning) {
             CancelInvoke("SpawnObject");
         }
