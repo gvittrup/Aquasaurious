2122b847fb1666dd4f5f8b944a7d5ec60924c635
gvittrup <gunnar.vittrup@gmail.com>
2023-07-28 12:56:11 -0600

    Fixes # 37 - Shark Movement + Rotation

--- Aquasaurious/Assets/Scripts/PlayerMovement.cs
+++ Aquasaurious/Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,8 @@ public class PlayerMovement : MonoBehaviour
 
     private CharacterController controller;
     private Vector3 playerVelocity;
-    private bool groundedPlayer;
     private float playerSpeed = 5.0f;
-    private float jumpHeight = 1.0f;
-    private float gravityValue = -9.81f;
+    private float rotationSpeed = 720f;
 
 
     private void Start()
@@ -23,12 +21,16 @@ public class PlayerMovement : MonoBehaviour
 
         /* This handles the physical movement of the gameobject */
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        move.Normalize();
+
         controller.Move(move * Time.deltaTime * playerSpeed);
 
         /* This handles the rotation of the game object */
         if (move != Vector3.zero)
         {
-            gameObject.transform.forward = move;
+            Quaternion toRotation = Quaternion.LookRotation(-move, Vector3.up);
+            Debug.Log(transform.rotation);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
 
     }
