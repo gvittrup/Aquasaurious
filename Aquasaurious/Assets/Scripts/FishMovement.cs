a0a2c8f4eec01b41d58091ba28224edfd651171d
gvittrup <gunnar.vittrup@gmail.com>
2023-08-07 22:45:43 -0600

    Health + SceneComponent + Optimization

--- Aquasaurious/Assets/Scripts/FishMovement.cs
+++ Aquasaurious/Assets/Scripts/FishMovement.cs
@@ -9,15 +9,21 @@ public class FishMovement : MonoBehaviour
     public float speed = 5.0f;
     private Rigidbody rb;
     private Vector3 screenBounds;
-    // Start is called before the first frame update
+
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
         rb.velocity = new Vector3(-speed,0,0);
+
+        // This is to prevent objects from being static - adds atmospheric rotation
+        if(gameObject.CompareTag("SceneComponent")) { 
+            Vector3 torque = new Vector3(Random.Range(-25f, 25f), Random.Range(-25f, 25f), Random.Range(-25f, 25f)); 
+            rb.AddTorque(torque);
+        }
+
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,Screen.height,Camera.main.transform.position.z));
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(transform.position.x < screenBounds.x * 2) 
@@ -29,22 +35,54 @@ public class FishMovement : MonoBehaviour
         private void OnCollisionEnter(Collision collision)
     {
 
-        //If the collision object is by either a fish or a ground element (coral) and against the player
-        if(collision.gameObject.CompareTag("Player") && (gameObject.CompareTag("Fish")|| gameObject.CompareTag("Ground")))
-        {
-            Destroy(gameObject);
-        } else if(collision.gameObject.CompareTag("Player") && gameObject.CompareTag("KillFish")) //If KillFish and Fish collide 
-        {
-            Destroy(gameObject);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        } else if(collision.gameObject.CompareTag("KillFish") && gameObject.CompareTag("Fish")) //If KillFish and Fish collide
-        {
-            Destroy(gameObject);
-        } else if(collision.gameObject.CompareTag("SceneBoundary") && gameObject.CompareTag("SceneComponent")) {
-            rb.AddForce(-5.0f, 0f, 0f, ForceMode.Impulse);
+        //Outer Switch = Object Collided With
+        //Inner Switch = Object Colliding / Object Script Attached To
+        switch(collision.gameObject.tag) {
+            
+            //Collided with Player
+            case "Player":
+                switch(gameObject.tag) {
+                    case "Fish":
+                        Destroy(gameObject);
+                        break;
+                    case "Ground":
+                        collision.gameObject.GetComponent<PlayerMovement>().Debuff();
+                        Destroy(gameObject);
+                        break;
+                    case "KillFish":
+                        Destroy(gameObject);
+                        collision.gameObject.GetComponent<PlayerMovement>().End();
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            
+            //Collided w/ Kill Fish
+            case "KillFish":
+                switch(gameObject.tag) {
+                    case "Fish":
+                        Destroy(gameObject);
+                        break;
+                    default:
+                        break;
+                }
+                break;
+
+            //Collided w/ Scene Boundary (Water / Ground)
+            case "SceneBoundary":
+                switch(gameObject.tag) {
+                    case "SceneComponent":
+                        rb.AddForce(-5.0f, 0f, 0f, ForceMode.Impulse);
+                        break;
+                    default:
+                        break;
+                }
+                break;
+
+            default:
+                break;
         }
-
-
 
     }
 }
--- Aquasaurious/Assets/Scripts/PlayerMovement.cs
+++ Aquasaurious/Assets/Scripts/PlayerMovement.cs
@@ -2,20 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    // private CharacterController controller;
     private Rigidbody rb;
     private Vector3 playerVelocity;
     private float playerSpeed = 5.0f;
+    private float speedConstant = 0.0f;
+    private float health = 1.0f;
     private float rotationSpeed = 720f;
+    private float frameCount = 0.0f;
+
     public PlayerControls playerControls;
     private InputAction swim;
 
+
     private void Awake() {
         playerControls = new PlayerControls();
+        speedConstant = playerSpeed;
     }
 
     private void OnEnable() {
@@ -35,6 +41,8 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
 
+        if(health <= 0) End();
+
         /* This handles the physical movement of the gameobject */
         Vector2 input = swim.ReadValue<Vector2>();
         Vector3 move = new Vector3(input.x, input.y, 0);
@@ -51,6 +59,25 @@ public class PlayerMovement : MonoBehaviour
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
 
+    }
+
+    void FixedUpdate() {
+        if(playerSpeed < speedConstant) {
+            playerSpeed = Mathf.Lerp(playerSpeed, speedConstant, frameCount/720.0f);
+            frameCount++;
+        } else {
+            health = 1.0f;
+        }
+    }
+
+    public void Debuff() {
+        playerSpeed = 1.0f;
+        frameCount = 0.0f;
+        health -= 0.5f;
+    }
+
+    public void End() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
 
